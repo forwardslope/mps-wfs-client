a3c3ee4474dfa222a433a293b790029e88ae8fbf
dmcintosh <dmcintosh@forwardslope.com>
2016-09-03 10:13:26 -0700

    Ticket #DCGSNSAFE-7709 Initial geoserver plug-in WFS implementation working Initial direct integration with GeoServer Catalog working

---
+++ src/main/java/com/fsi/geomap/mps/wfsclient/WfsClient.java
@@ -0,0 +1,14 @@
+package com.fsi.geomap.mps.wfsclient;
+
+import java.util.Map;
+
+import org.geotools.feature.FeatureCollection;
+import org.opengis.feature.Feature;
+import org.opengis.feature.type.FeatureType;
+import org.opengis.feature.type.Name;
+
+public interface WfsClient {
+	public Map<Name, FeatureCollection<? extends FeatureType, ? extends Feature>> getFeatures(GetFeature getFeature);
+	public FeatureCollection<? extends FeatureType, ? extends Feature> getFeatures(FeatureType featureType, GetFeature getFeature);
+
+}
