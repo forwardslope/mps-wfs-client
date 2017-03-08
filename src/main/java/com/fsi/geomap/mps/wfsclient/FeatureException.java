d60f8deea5a43e10f860f6c2ebc7c8e7a17fdee5
dmcintosh <dmcintosh@forwardslope.com>
2017-03-08 13:38:16 -0800

    Ticket #MSCM-171 Resolve CAT I and CAT II findings

---
+++ src/main/java/com/fsi/geomap/mps/wfsclient/FeatureException.java
@@ -0,0 +1,29 @@
+package com.fsi.geomap.mps.wfsclient;
+
+public class FeatureException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FeatureException() {
+	}
+
+	public FeatureException(String arg0) {
+		super(arg0);
+	}
+
+	public FeatureException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public FeatureException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public FeatureException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+	}
+
+}
--- src/main/java/com/fsi/geomap/mps/wfsclient/WfsNotifier.java
+++ src/main/java/com/fsi/geomap/mps/wfsclient/WfsNotifier.java
@@ -1,11 +1,16 @@
 package com.fsi.geomap.mps.wfsclient;
 
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.opengis.feature.Feature;
 import org.opengis.feature.type.Name;
 import org.opengis.filter.Filter;
+import org.xml.sax.SAXException;
 
 public interface WfsNotifier {
-	public void processNotification(Name name,  Feature feature) throws Exception;
+	public void processNotification(Name name,  Feature feature) throws IOException, SAXException, ParserConfigurationException, FeatureException;
 	public void processDeleteNotification(Name name, Filter filter);
 
 }
